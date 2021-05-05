ce07e2c2edc85c510463a2932c8b8ad2c1a78078
Yuval <yuval175@gmail.com>
2021-05-05 15:39:51 +0300

    fix debug

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,8 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('', include('users.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve,
-            {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
