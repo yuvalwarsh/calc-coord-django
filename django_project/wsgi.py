b641d8b1825621958c1206c2108dc0fbf6aa7f5c
Yuval <yuval175@gmail.com>
2021-05-06 04:07:23 +0300

    fix debug

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
