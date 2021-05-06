8982ae98731c2e27a9ae63140a145cf8afbc6a22
Yuval <yuval175@gmail.com>
2021-05-06 05:01:50 +0300

    fix debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'build/static'),
+    os.path.join(BASE_DIR, 'build')
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
