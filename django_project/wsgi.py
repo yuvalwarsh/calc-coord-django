1a1a82d865527cf184c0ca6b832b75391b5b9eae
Yuval <yuval175@gmail.com>
2021-05-06 04:07:06 +0300

    fix debug

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -15,6 +15,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/static/')
-application.add_files('/static/', prefix='more-files/')
 
