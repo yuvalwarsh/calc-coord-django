3d1b54e63f044232c11c79f77f3798c661693b67
Yuval <yuval175@gmail.com>
2021-05-06 04:47:14 +0300

    fix debug

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views as main_app_views
 
 urlpatterns = [
-    path('', main_app_views.home, name='homepage'),
+    path('/', main_app_views.home, name='homepage'),
     path('pts/<newdoc_uuid>/', main_app_views.pts, name='pts'),
     path('links/<newdoc_uuid>/', main_app_views.links, name='links'),
     path('users/<str:username>/points-list/', main_app_views.UserPointsListView.as_view(), name='user-points'),
