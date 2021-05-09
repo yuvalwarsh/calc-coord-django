d241f4b238f3826e96f321e0eae8fdd3315395a1
Yuval <yuval175@gmail.com>
2021-05-09 23:57:05 +0300

    add download links file

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,7 +133,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_URL = 'http://calc-coord-django-files-bucket.s3.amazonaws.com/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
